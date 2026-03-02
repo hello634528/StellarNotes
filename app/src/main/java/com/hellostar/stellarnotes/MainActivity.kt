17ba88de1a23011788d393444f6a6d8301bd8f04
hello634528 <3252622684@qq.com>
2026-03-02 10:01:36 +0800

    feat: pass AppPreferences to StellarApp

--- app/src/main/java/com/hellostar/stellarnotes/MainActivity.kt
+++ app/src/main/java/com/hellostar/stellarnotes/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.room.Room
 import com.hellostar.stellarnotes.data.NoteDatabase
 import com.hellostar.stellarnotes.data.NoteRepository
+import com.hellostar.stellarnotes.ui.AppPreferences
 import com.hellostar.stellarnotes.ui.StellarApp
 import com.hellostar.stellarnotes.ui.StellarViewModel
 import com.hellostar.stellarnotes.ui.StellarViewModelFactory
@@ -18,11 +19,14 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent { AppContent() }
+        
+        val appPrefs = AppPreferences(applicationContext)
+        
+        setContent { AppContent(appPrefs) }
     }
 
     @Composable
-    private fun AppContent() {
+    private fun AppContent(appPrefs: AppPreferences) {
         val db = remember {
             Room.databaseBuilder(
                 applicationContext,
@@ -34,6 +38,6 @@ class MainActivity : ComponentActivity() {
         val vm: StellarViewModel = viewModel(
             factory = StellarViewModelFactory(repo)
         )
-        StellarApp(viewModel = vm)
+        StellarApp(viewModel = vm, appPrefs = appPrefs)
     }
 }
